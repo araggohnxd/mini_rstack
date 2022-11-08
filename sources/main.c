331ac0677adfe381c423fc2284cc97d50d05c357
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2022-11-08 18:29:09 -0300

    destroy mlx if parsing fails

--- sources/main.c
+++ sources/main.c
@@ -6,7 +6,7 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/05 12:23:43 by maolivei          #+#    #+#             */
-/*   Updated: 2022/11/07 12:52:37 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/11/08 18:28:01 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,12 +21,18 @@ int	main(int argc, char **argv)
 	if (!get_mlx()->mlx)
 		return (error("Unable to create MLX display."), 1);
 	if (check_user_input(argc) != 0)
-		return (1);
+		return (destroy_mlx(), 1);
 	ft_bzero(&scene, sizeof(t_rt_scene));
 	if (read_rt_file(argv[1], &scene) != 0)
-		return (destroy_scene(&scene), 1);
+	{
+		destroy_scene(&scene);
+		return (destroy_mlx(), 1);
+	}
 	if (scene_to_world(&rt, &scene) != 0)
-		return (destroy_minirt(&scene, &rt), 1);
+	{
+		destroy_minirt(&scene, &rt);
+		return (destroy_mlx(), 1);
+	}
 	destroy_minirt(&scene, &rt);
 	setup_visual_environment(&rt);
 	return (0);
