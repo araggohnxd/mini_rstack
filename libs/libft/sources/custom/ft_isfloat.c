5d04453e0fa8046a68c0609c4e7a76c2747c311d
Matheus Oliveira <31555284+araggohnxd@users.noreply.github.com>
2022-11-08 17:21:05 -0300

    update libft

--- libs/libft/sources/custom/ft_isfloat.c
+++ libs/libft/sources/custom/ft_isfloat.c
@@ -5,24 +5,57 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/11/04 10:32:25 by maolivei          #+#    #+#             */
-/*   Updated: 2022/11/04 10:52:42 by maolivei         ###   ########.fr       */
+/*   Created: 2022/11/08 00:48:36 by maolivei          #+#    #+#             */
+/*   Updated: 2022/11/08 01:39:43 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+static t_bool	check_start(const char *str, size_t *index, t_bool *dot)
+{
+	if (str[0] == '.' || str[0] == '-' || str[0] == '+')
+	{
+		if (!str[1])
+			return (FALSE);
+		if (str[0] == '.')
+			*dot = TRUE;
+		++(*index);
+	}
+	return (TRUE);
+}
+
+static t_bool	check_dot(const char *str, size_t *index, t_bool *dot)
+{
+	if (*dot)
+		return (FALSE);
+	if (!ft_isdigit(str[*index + 1]))
+		if (!ft_isdigit(str[*index - 1]))
+			return (FALSE);
+	++(*index);
+	*dot = TRUE;
+	return (TRUE);
+}
+
 t_bool	ft_isfloat(const char *str)
 {
-	if (*str == '+' || *str == '-')
-		++str;
-	while (*str && ft_isdigit(*str))
-		++str;
-	if (*str == '.')
-		++str;
-	while (*str && ft_isdigit(*str))
-		++str;
-	if (*str && !ft_isdigit(*str))
+	size_t	index;
+	t_bool	dot;
+
+	index = 0;
+	dot = FALSE;
+	if (!check_start(str, &index, &dot))
 		return (FALSE);
+	while (str[index])
+	{
+		if (str[index] == '.')
+			if (!check_dot(str, &index, &dot))
+				return (FALSE);
+		if (str[index] && !ft_isdigit(str[index]))
+			return (FALSE);
+		if (!str[index])
+			break ;
+		++index;
+	}
 	return (TRUE);
 }
--- libs/libft/sources/custom/ft_isnumber.c
+++ libs/libft/sources/custom/ft_isnumber.c
@@ -6,7 +6,7 @@
 /*   By: maolivei <maolivei@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/11/04 10:32:25 by maolivei          #+#    #+#             */
-/*   Updated: 2022/11/04 10:52:42 by maolivei         ###   ########.fr       */
+/*   Updated: 2022/11/08 14:48:03 by maolivei         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,8 @@
 t_bool	ft_isnumber(const char *str)
 {
 	if (*str == '+' || *str == '-')
-		++str;
+		if (!*(++str))
+			return (FALSE);
 	while (*str)
 		if (!ft_isdigit(*str++))
 			return (FALSE);
